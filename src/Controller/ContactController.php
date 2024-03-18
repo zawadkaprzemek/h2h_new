b846623da5461c70e0fd8716ae510ad868a522d2
przemyslaw <zawadkaprzemek@gmail.com>
2024-03-18 22:49:25 +0100

    Controller and Form

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ContactMessage;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/contact')]
+class ContactController extends AbstractController
+{
+
+    public function __construct(readonly EntityManagerInterface $entityManager)
+    {
+    }
+
+
+    #[Route('/form', name: 'app_contact_form', methods: ['POST'])]
+    public function formAction(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $form = $this->createForm(ContactType::class);
+        $form->submit($data);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactMessage = $form->getData();
+            $this->entityManager->persist($contactMessage);
+            $this->entityManager->flush();
+            return $this->json([
+                'message' => 'Dziękujemy za przesłaną wiadomość, niedługo odpowiemy na Państwa wiadomość ;)',
+            ], Response::HTTP_CREATED);
+        }
+
+        $errors = $this->getErrorsFromForm($form);
+
+        return $this->json([
+            'errors' => $errors,
+        ], Response::HTTP_BAD_REQUEST);
+    }
+
+    #[Route('/list', name: 'app_contact_list', methods: ['GET'])]
+    public function listAction(): JsonResponse
+    {
+        $list = $this->entityManager->getRepository(ContactMessage::class)->findAll();
+
+        return $this->json($list);
+    }
+
+
+    private function getErrorsFromForm(FormInterface $form): array
+    {
+        $errors = [];
+
+        foreach ($form->getErrors(true) as $error) {
+            $errors[] = $error->getMessage();
+        }
+        foreach ($form->all() as $childForm) {
+            if ($childForm instanceof FormInterface) {
+                if ($childErrors = $this->getErrorsFromForm($childForm)) {
+                    $errors[$childForm->getName()] = $childErrors;
+                }
+            }
+        }
+
+        return $errors;
+    }
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ContactMessage;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fullName', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('message', TextareaType::class)
+            ->add('consent', CheckboxType::class, [
+                'required' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContactMessage::class,
+        ]);
+    }
+}
